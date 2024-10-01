353adfc73ed8fb97ddc63f655cb5ea5afe8a414b
JavierM10 <javerlot7mb@gmail.com>
2024-10-01 18:18:32 +0200

    average_sorting_time done

--- times.c
+++ times.c
@@ -11,6 +11,8 @@
 
 #include "times.h"
 #include "sorting.h"
+#include "permutations.h"
+#include <time.h>
 
 /***************************************************/
 /* Function: average_sorting_time Date:            */
@@ -22,7 +24,37 @@ short average_sorting_time(pfunc_sort metodo,
                               int N, 
                               PTIME_AA ptime)
 {
-  /* Your code */
+  int **matrix = NULL, i, current_ob = 0, min_ob = 0, max_ob = 0, n_ob = 0;
+  double total_ob = 0.0, avg_time = 0.0;
+  clock_t start_time, end_time;
+
+  if(!metodo || n_perms <= 0 || N <= 0 || !ptime) return ERR;
+
+  ptime->n_elems = n_perms;
+  ptime->N = N;
+
+  if(!(matrix = generate_permutations(n_perms, N))) return ERR;
+
+  start_time = clock(); /*Get the initial time*/
+
+  for(i=0; i<n_perms; i++){
+    if((current_ob = metodo(matrix[i], matrix[i][0], matrix[i][N]))== ERR) {break; return ERR;}
+    if(i=0) min_ob = max_ob = current_ob;
+    else if (current_ob < min_ob) min_ob = current_ob;
+    else if (current_ob > max_ob) max_ob = current_ob;
+    total_ob += current_ob;
+    n_ob++;
+  }
+
+  end_time = clock(); /*Get the last time*/
+  avg_time = (double)(end_time - start_time) / CLOCKS_PER_SEC; /*Calculate the average time. We pass from ticks to seconds dividing by CLOCKS_PER_SEC*/
+  
+  ptime->time = avg_time;
+  ptime->average_ob = total_ob/n_ob;
+  ptime->min_ob = min_ob;
+  ptime->max_ob = max_ob;
+
+  return OK;
 }
 
 /***************************************************/
