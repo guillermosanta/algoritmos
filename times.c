781f15e2cd5632815c0e313e527dafc6de28fdcf
JavierM10 <javerlot7mb@gmail.com>
2024-10-02 14:35:33 +0200

    fixed

--- times.c
+++ times.c
@@ -24,7 +24,7 @@ short average_sorting_time(pfunc_sort metodo,
                               int N, 
                               PTIME_AA ptime)
 {
-  int **matrix = NULL, i, current_ob = 0, min_ob = 0, max_ob = 0, n_ob = 0;
+  int **matrix = NULL, i, current_ob = 0;
   double total_ob = 0.0, avg_time = 0.0;
   clock_t start_time, end_time;
 
@@ -35,24 +35,21 @@ short average_sorting_time(pfunc_sort metodo,
 
   if(!(matrix = generate_permutations(n_perms, N))) return ERR;
 
-  start_time = clock(); /*Get the initial time*/
-
   for(i=0; i<n_perms; i++){
-    if((current_ob = metodo(matrix[i], matrix[i][0], matrix[i][N]))== ERR) {break; return ERR;}
-    if(i=0) min_ob = max_ob = current_ob;
-    else if (current_ob < min_ob) min_ob = current_ob;
-    else if (current_ob > max_ob) max_ob = current_ob;
+    start_time = clock(); /*Get the initial time*/
+    
+    if((current_ob = metodo(matrix[i], 0, N-1))== ERR) return ERR;
+    if(i=0) ptime->min_ob = ptime->max_ob = current_ob;
+    else if (current_ob < ptime->min_ob) ptime->min_ob = current_ob;
+    else if (current_ob > ptime->max_ob) ptime->max_ob = current_ob;
     total_ob += current_ob;
-    n_ob++;
-  }
 
-  end_time = clock(); /*Get the last time*/
-  avg_time = (double)(end_time - start_time) / CLOCKS_PER_SEC; /*Calculate the average time. We pass from ticks to seconds dividing by CLOCKS_PER_SEC*/
+    end_time = clock(); /*Get the last time*/
+    avg_time += (double)(end_time - start_time) / CLOCKS_PER_SEC; /*Calculate the average time per interaction. We pass from ticks to seconds dividing by CLOCKS_PER_SEC*/
+  }
   
-  ptime->time = avg_time;
-  ptime->average_ob = total_ob/n_ob;
-  ptime->min_ob = min_ob;
-  ptime->max_ob = max_ob;
+  ptime->time = avg_time/n_perms;
+  ptime->average_ob = total_ob/n_perms;
 
   return OK;
 }
